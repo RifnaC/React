07703560f9ada730129ad0a3296fa7afc95b0bd5
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-03-26 14:58:26 +0530

    update the code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // single element
 const  heading = React.createElement("h1", {id:"Heading"}, "Hello, React "); 
 const root = ReactDOM.createRoot(document.getElementById("root"));
